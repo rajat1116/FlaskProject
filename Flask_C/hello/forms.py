fd374f2807c3d6d6ad224fd0ea64f9644d0cb358
rajat1116 <rahulpandit106@gmail.com>
2019-07-22 14:05:41 +0530

    Add files via upload

--- Flask_C/hello/forms.py
+++ Flask_C/hello/forms.py
@@ -35,10 +35,10 @@ class CandidateForm(FlaskForm):
     skills = StringField('Skills', validators=[DataRequired()])
     source = StringField('Source', validators=[DataRequired()])
     cv = FileField('Upload Resume', validators=[DataRequired(), FileAllowed(['png', 'jpg', 'pdf', 'docx'])])
-    choices = [('01', 'Data Engineer'), ('02', 'UI/UX dev'), ('04', 'Full Stack'),
-               ('05', 'AndriodDevloper'), ('06', 'IosDev'), ('07', 'HR'), ('08', 'Salesforce'),
-               ('09', 'Anaplan')]
-    jobId = SelectField('Select Job ID', choices=choices)
+    ch = [('01', 'Data Engineer'), ('02', 'UI/UX dev'), ('03', 'Associates'),
+          ('04', 'Full Stack'), ('05', 'Andriod Devloper'), ('06', 'Ios Dev'), ('07', 'HR'),
+          ('08', 'Salesforce'), ('09', 'Anaplan'), ('10', 'Backend')]
+    jobId = SelectField('Select Job ID', choices=ch)
     submit = SubmitField('Submit')
 
 
@@ -59,9 +59,9 @@ class ProgessTrack(FlaskForm):
 
 
 class JobVacancy(FlaskForm):
-    choices = [('01', 'Data Engineer'), ('02', 'UI/UX dev'), ('03', 'Full Stack'),
-               ('04', 'AndriodDevloper'), ('05', 'Ios Dev'), ('06', 'HR'), ('07', 'Salesforce'),
-               ('08', 'Anaplan'), ('09', 'Associates'), ('10', 'Backend')]
-    selectVac = SelectField('Select Vacancy: ', choices=choices)
+    ch = [('01', 'Data Engineer'), ('02', 'UI/UX dev'), ('03', 'Associates'),
+          ('04', 'Full Stack'), ('05', 'Andriod Devloper'), ('06', 'Ios Dev'), ('07', 'HR'),
+          ('08', 'Salesforce'), ('09', 'Anaplan'), ('10', 'Backend')]
+    selectVac = SelectField('Select Vacancy: ', choices=ch)
     NVacancy = IntegerField('Number Of Vacancy: ')
     submit = SubmitField('Submit')
